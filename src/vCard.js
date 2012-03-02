cf82e481b4d233b340afed7b4ee473fb9b75906e
Eivind Ingebrigtsen <eivind@muff.no>
2012-03-02 19:40:40 +0100

    output

--- src/vCard.js
+++ src/vCard.js
@@ -1,11 +1,8 @@
-
-
 var vCardParser = function(input){
-
 		var props = /type=(.*?)[:;]/gi,
 			prop = /type=(.*?)[:;]/i,
 			stripProps = function(str){
-
+				if(!str){return str;}
 				var a,
 					m = str.match(props),
 					properties = [];
@@ -14,6 +11,9 @@ var vCardParser = function(input){
 						properties.push(m[a].match(prop)[1]);
 						str = str.replace(m[a], '');
 					}
+				}
+				if(!properties.length){
+					properties = null;
 				}
 				return {
 					value: str,
@@ -28,6 +28,14 @@ var vCardParser = function(input){
 			}
 			return res;
 		};
+		var Name = function(match){
+			var a, res = {}, namefields = ['Lastname','Firstname','Middlename1', 'Middlename2','Middlename3'];
+			for(a in match){
+				res[namefields[a]] = match[a].match(/[\w\d]/) ? match[a] : '';
+			}
+			console.log(match, res);
+			return res;
+		};
 		var Phone = function(match){
 			return stripProps(match);
 		};
@@ -35,13 +43,25 @@ var vCardParser = function(input){
 		var Email = function(match){
 			return stripProps(match);
 		};
+		
 		var Network = function(match){
-			return match;
+			return {
+				url: match[2],
+				type: match[1]
+			};
+		};
+
+		var Photo = function(match){
+			var encoding, data, types = /(png|jpeg|jpg|gif)/i;
+				encoding = match[2].match(types)[1].toLowerCase();
+				data = match[3];
+				return 'data:image/'+encoding+';base64'+data;	
 		};
 
 		var Card = function(){
 			return {
 				'name': null,
+				'names': null,
 				'id': null,
 				'networks': [],
 				'image': null,
@@ -53,16 +73,12 @@ var vCardParser = function(input){
 				'urls': []
 			};
 		};
-		var Photo = function(match){
-			var encoding, data, types = /(png|jpeg|jpg|gif)/i;
-				encoding = match[2].match(types)[1].toLowerCase();
-				data = match[3];
-				return 'data:image/'+encoding+';base64'+data;	
-		};
-
+		
 		var self = {};
 			self.state = { view: 'list'};
-			self.to_html= function(tmpl, data, options){};
+			self.to_html= function(tmpl, data, options){
+				return Mustache.to_html(tmpl, data);
+			};
 			self.to_json= function(data,options){};
 			self.parse= function(str){
 				self.cards = str.split('END:VCARD');
@@ -71,6 +87,9 @@ var vCardParser = function(input){
 				for(a in c){
 					d.push(r(c[a]));
 				}
+				//console.log(self.to_html(window.template, {cards: d}));
+				$('#content').html(self.to_html(window.template, {cards: d}));				
+
 			};
 			self.translate = {
 				"FN": "name",
@@ -79,45 +98,65 @@ var vCardParser = function(input){
 				"EMAIL": 'email',
 				"PHOTO": 'image',
 				"BDAY": 'birthdate',
-				"URL": 'urls'
+				"URL": 'urls',
+				"ROLE": 'role',
+				"TEL": 'phone',
+				"N": 'names'
 			};
 			self.listenFor = {
-				"VERSION FN ORG TITLE": function(match){
-					return  {
-						key: self.translate[match[1]] || match[1],
-						value: match[2]
+				"N": function (match){
+					console.log(match);
+					var name = match[2].split(';');
+					return {
+						key: 'names',
+						value: Name(name)
 					};
 				},
-				"PHOTO": function (match) {
+				"VERSION FN ORG URL TEL ROLE TITLE": function(match){
+					return  {
+						key: self.translate[match[1]] || match[1],
+						value: stripProps(match[2])
+					};
+				},
+				"PHOTO LOGO": function (match) {
 					return {
 						key: 'image',
 						value: Photo(match)
 					};
 				},
+				"X-ABLabel":function(match, prev){
+					if(typeof prev == 'object'){						
+						prev.label = match[2];
+					}else{
+						prev = {
+							value: prev,
+							label: match[2]
+						};
+					}
+					return false;
+				},
 				"EMAIL BDAY": function  (match) {
 					return  {
 						key: self.translate[match[1]] || match[1],
-						value: match[3]
-					};
-				},
-				"URL": function(match){
-					return {
-						key: 'urls',
-						value: stripProps(match[2])
+						value: Email(match[3])
 					};
 				},
 				"ADR": function (match){
 					var ad = match[3].split('type=pref:');
+					var pref = ad.length > 1;
 					var address = ad.toString().split(';');
 					return {
+						pref: pref,
 						key: 'address',
 						value: Address(address)
 					};
 				},
+				
+
 				"twitter facebook linkedin github":function(match){
 					return {
 						key: 'networks',
-						value: Network(match[2])
+						value: Network(match)
 					};
 				}
 			};
@@ -128,11 +167,12 @@ var vCardParser = function(input){
 					c, // listen iterator
 					m, // current match
 					r, // current translation results
+					prev, // previous object, in case of labels
 					d = new Card(), // for storing data
 					l = self.listenFor;
 
 				var x = {
-					'simple': /^(version|fn|title|org)\:(.+)$/i
+					'simple': /^(version|fn|n|title|org)\:(.+)$/i
 ,					'items': /^item\d{1,2}\.(.*?)[;:]type=(.*?);(.*?)$/i
 ,					'itm': /^item\d{1,2}\.(.*?)[;:](.*?)$/i
 ,					'email': /^(ÊMAIL);.*?\:(.*?@.*?)$/i
@@ -148,11 +188,14 @@ var vCardParser = function(input){
 						if(m && m.length){
 							for(c in l){
 								if(c.match && c.match(m[1])){
-									r = l[c](m);
-									if(d[r.key] && typeof d[r.key] == 'object'){
-										d[r.key].push(r.value);
+									r = l[c](m, prev);
+									
+									if(r && d[r.key] && typeof d[r.key] == 'object'){
+									d[r.key].push(r.value);
+									prev = d[r.key][(d[r.key].length-1)];
 									}else{
 										d[r.key] = r.value;
+										prev = d[r.key];
 									}
 									continue lineloop;
 								}
